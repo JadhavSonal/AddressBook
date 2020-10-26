e966685882084786eeadd25c85996dbec06e01a9
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-26 12:16:12 +0530

    Added:Surface of the building exposed to sunlight

---
+++ Polygon1.java
@@ -0,0 +1,21 @@
+class Point
+{
+    int x, y;
+ 
+    Point()
+    {}
+ 
+    Point(int p, int q)
+    {
+        x = p;
+        y = q;
+    }
+}
+class Polygon1
+{
+	public void  fun(int p,int q)
+	{
+		int x=p;
+		int y=q;
+	}
+	
